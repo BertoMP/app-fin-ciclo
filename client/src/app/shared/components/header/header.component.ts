dc7d94d9548886edf007198b0698aeb498adeabf
Alberto Martínez Pérez <alberto.martinezperez@hotmail.es>
2024-04-03 18:47:09 +0200

    Header y footer

--- client/src/app/shared/components/header/header.component.ts
+++ client/src/app/shared/components/header/header.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {NavBarComponent} from "../nav-bar/nav-bar.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    NavBarComponent
+    NavBarComponent,
+    RouterLink
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
