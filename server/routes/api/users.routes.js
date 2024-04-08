7d77c456acba5363d1beaf36140302269ae0b2b6
Alberto Martínez Pérez <alberto.martinezperez@hotmail.es>
2024-04-08 22:01:21 +0200

    Servidor: Implementación de JWT. Implementación de CRUD para Medicamento. Implementación de Login y Registro.

--- server/routes/api/users.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const userController = require('../../controllers/user.controller');
-
-// Rutas POST
-router.post('/login', userController.postLogin);
-router.post('/register', userController.postRegister);
-
-module.exports = router;
