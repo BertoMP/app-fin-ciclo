e02b2b6319d4c39012b1894481956e85e1825bf0
Alberto Martínez Pérez <115874159+BertoMP@users.noreply.github.com>
2024-04-30 12:27:13 +0200

    Servidor: Modificación del fichero app.js.

--- server/app.js
+++ server/app.js
@@ -1,31 +1,40 @@
+// Cargamos las variables de entorno
+require('dotenv').config();
+
+// Importamos las dependencias necesarias
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.js');
 const express = require('express');
 const cors = require('cors');
 
-require('dotenv').config();
-
-const SERV_HOST = process.env.SERV_HOST;
-const SERV_PORT = process.env.SERV_PORT;
-
-const app = express();
-
+// Configuramos las opciones de CORS
 const corsOptions = {
   origin: process.env.CORS_ORIGIN,
   methods: process.env.CORS_METHODS,
   allowedHeaders: process.env.CORS_ALLOWED_HEADERS
 };
 
-app.use(cors(corsOptions));
+// Definimos las variables de entorno para el host y el puerto del servidor
+const SERV_HOST = process.env.SERV_HOST;
+const SERV_PORT = process.env.SERV_PORT;
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./docs/swagger.js');
+// Creamos la aplicación Express
+const app = express();
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+// Configuramos el middleware de Express para analizar el cuerpo de las solicitudes
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
 
+// Aplicamos las opciones de CORS a nuestra aplicación Express
+app.use(cors(corsOptions));
+
+// Configuramos Swagger UI para servir la documentación de la API
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Importamos y usamos nuestras rutas de API
 app.use('/api', require('./routes/api'));
 
+// Iniciamos el servidor
 app.listen(SERV_PORT, SERV_HOST, () => {
   console.log(`NodeJS Server listening on http:\\${SERV_HOST}:${SERV_PORT}`);
 });
