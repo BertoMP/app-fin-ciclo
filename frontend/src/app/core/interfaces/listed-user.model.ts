d4f2421adb3d4eddd455bfbc5e26634e9ea35435
Alberto Martínez Pérez <alberto.martinezperez@hotmail.es>
2024-05-19 13:49:29 +0200

    Cliente: Cambiado estilado de listado de usuarios.

--- frontend/src/app/core/interfaces/listed-user.model.ts
+++ frontend/src/app/core/interfaces/listed-user.model.ts
@@ -4,5 +4,6 @@ import {RolDataModel} from "./rol-data.model";
 export interface ListedUserModel {
   usuario_id: number,
   datos_personales: PersonalDataModel,
-  datos_rol: RolDataModel
+  datos_rol: RolDataModel,
+  datos_turno?: string
 }
