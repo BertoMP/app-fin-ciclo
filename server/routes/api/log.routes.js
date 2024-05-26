5da1c89c99505762427093b2268ee8c795f358f1
Alberto Martínez Pérez <alberto.martinezperez@hotmail.es>
2024-05-26 17:03:57 +0200

    Servidor: Comentario swagger.

--- server/routes/api/log.routes.js
+++ server/routes/api/log.routes.js
@@ -14,10 +14,49 @@ import { verifyUserId } from '../../util/middleware/verifyUserId.js';
 import { validateQueryParams } from "../../helpers/validators/queryParams/queryParams.validator.js";
 
 // Rutas GET
+/**
+ * @swagger
+ * /log:
+ *   get:
+ *     summary: Obtiene todos los logs de la base de datos
+ *     description: Devuelve un objeto JSON con la respuesta HTTP que incluye los datos de los logs. Si ocurre algún error durante el proceso, devuelve un error con el mensaje correspondiente.
+ *     tags: [Logs]
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *         description: La página de logs que se desea obtener.
+ *       - in: query
+ *         name: fechaInicio
+ *         schema:
+ *           type: string
+ *         description: La fecha de inicio para filtrar los logs.
+ *       - in: query
+ *         name: fechaFin
+ *         schema:
+ *           type: string
+ *         description: La fecha de fin para filtrar los logs.
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *         description: El número de logs a mostrar por página.
+ *     responses:
+ *       200:
+ *         description: La operación fue exitosa y devuelve los logs correspondientes.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/LogPaginado'
+ *       500:
+ *         description: Ocurrió un error durante el proceso y se devuelve un mensaje de error.
+ */
 router.get(
 	'/log',
 	verifyAccessToken,
 	verifyUserRole([1]),
+	verifyUserId,
 	validateQueryParams,
 	LogController.getLogs
 );
