e9af4c48dc9ab155db2fb21eb338b2a3961333e4
Alberto Martínez Pérez <alberto.martinezperez@outlook.com>
2024-06-05 13:04:11 +0200

    Cliente: Eliminados ficheros de spec

--- frontend/src/app/shared/pipes/remove-br.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RemoveBrPipe } from './remove-br.pipe';
-
-describe('RemoveBrPipe', () => {
-  it('create an instance', () => {
-    const pipe = new RemoveBrPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
